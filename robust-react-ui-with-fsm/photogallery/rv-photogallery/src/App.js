6188471151604f03966006b669ab839e9550b8df
Herve Desrosiers <hdesrosiers@mtlt30rjg5l-mac.corp.stingraydigital.com>
2019-04-09 07:23:23 -0400

    fetchJsonp working

--- robust-react-ui-with-fsm/photogallery/rv-photogallery/src/App.js
+++ robust-react-ui-with-fsm/photogallery/rv-photogallery/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import galleryMachine from "./galleryMachine.js";
+import fetchJsonp from "fetch-jsonp";
 
 class App extends Component {
   state = {
@@ -8,6 +9,7 @@ class App extends Component {
     items: []
   };
 
+  /////  TRANSITIONS /////
   // retrieve current finite state
   transition(action) {
     const currentGalleryState = this.state.gallery;
@@ -23,6 +25,7 @@ class App extends Component {
     }
   }
 
+  ///// COMMANDS /////
   // When a state change occurs, commands might be executed.
   command(nextState, action) {
     switch (nextState) {
@@ -46,9 +49,115 @@ class App extends Component {
         break;
     }
   }
+  //////////////////
+  handleSubmit(e) {
+    e.persist();
+    e.preventDefault();
 
+    this.transition({ type: "SEARCH", query: this.state.query });
+  }
+
+  search(query) {
+    const encodedQuery = encodeURIComponent(query);
+
+    setTimeout(() => {
+      fetchJsonp(
+        `https://api.flickr.com/services/feeds/photos_public.gne?lang=en-us&format=json&tags=${encodedQuery}`,
+        { jsonpCallback: "jsoncallback" }
+      )
+        .then(res => res.json())
+        .then(data => {
+          this.transition({ type: "SEARCH_SUCCESS", items: data.items });
+        })
+        .catch(error => {
+          this.transition({ type: "SEARCH_FAILURE" });
+        });
+    }, 1000);
+  }
+  handleChangeQuery(value) {
+    this.setState({ query: value });
+  }
+  renderForm(state) {
+    const searchText =
+      {
+        loading: "Searching...",
+        error: "Try search again",
+        start: "Search"
+      }[state] || "Search";
+
+    return (
+      <form className="ui-form" onSubmit={e => this.handleSubmit(e)}>
+        <input
+          type="search"
+          className="ui-input"
+          value={this.state.query}
+          onChange={e => this.handleChangeQuery(e.target.value)}
+          placeholder="Search Flickr for photos..."
+          disabled={state === "loading"}
+        />
+        <div className="ui-buttons">
+          <button className="ui-button" disabled={state === "loading"}>
+            {searchText}
+          </button>
+          {state === "loading" && (
+            <button
+              className="ui-button"
+              type="button"
+              onClick={() => this.transition({ type: "CANCEL_SEARCH" })}
+            >
+              Cancel
+            </button>
+          )}
+        </div>
+      </form>
+    );
+  }
+  renderGallery(state) {
+    return (
+      <section className="ui-items" data-state={state}>
+        {state === "error" ? (
+          <span className="ui-error">Uh oh, search failed.</span>
+        ) : (
+          this.state.items.map((item, i) => (
+            <img
+              src={item.media.m}
+              className="ui-item"
+              style={{ "--i": i }}
+              key={item.link}
+              onClick={() =>
+                this.transition({
+                  type: "SELECT_PHOTO",
+                  item
+                })
+              }
+            />
+          ))
+        )}
+      </section>
+    );
+  }
+  renderPhoto(state) {
+    if (state !== "photo") return;
+
+    return (
+      <section
+        className="ui-photo-detail"
+        onClick={() => this.transition({ type: "EXIT_PHOTO" })}
+      >
+        <img src={this.state.photo.media.m} className="ui-photo" />
+      </section>
+    );
+  }
   render() {
-    return <div className="App" />;
+    const galleryState = this.state.gallery;
+
+    return (
+      <div className="ui-app" data-state={galleryState}>
+        {this.renderForm(galleryState)}
+        {this.renderGallery(galleryState)}
+        {this.renderPhoto(galleryState)}
+      </div>
+    );
   }
 }
 
--- robust-react-ui-with-fsm/photogallery/xstateMachine.js
+++ robust-react-ui-with-fsm/photogallery/xstateMachine.js
@@ -1,29 +1,49 @@
 // xstate machine
-const galleryMachine = Machine({
-  id: "gallery",
-  initial: "start",
-  states: {
-    start: {
-      on: { SEARCH: "loading" }
-    },
-    loading: {
-      on: {
-        SEARCH_SUCCESS: "gallery",
-        SEARCH_FAILURE: "error",
-        CANCEL_SEARCH: "gallery"
+const galleryMachine = Machine(
+  {
+    id: "gallery",
+    initial: "start",
+    states: {
+      start: {
+        on: {
+          SEARCH: {
+            target: "loading",
+            actions: ["search"] //transition action
+          }
+        }
+      },
+      loading: {
+        on: {
+          SEARCH_SUCCESS: "gallery",
+          SEARCH_FAILURE: "error",
+          CANCEL_SEARCH: "gallery"
+        }
+      },
+      error: {
+        on: { SEARCH: "loading" }
+      },
+      gallery: {
+        on: {
+          SEARCH: "loading",
+          SELECT_PHOTO: "photo"
+        }
+      },
+      photo: {
+        on: { EXIT_PHOTO: "gallery" }
+      }
+    }
+  },
+  // machine options
+  {
+    actions: {
+      search: () => {
+        console.log("searching...");
       }
     },
-    error: {
-      on: { SEARCH: "loading" }
-    },
-    gallery: {
-      on: {
-        SEARCH: "loading",
-        SELECT_PHOTO: "photo"
+    guards: {
+      validSearch: (ctx, event) => {
+        return ctx.canSearch && event;
       }
-    },
-    photo: {
-      on: { EXIT_PHOTO: "gallery" }
     }
   }
-});
+);
