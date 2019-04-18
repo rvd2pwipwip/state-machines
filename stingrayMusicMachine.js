23a614d23aee29fdb52d97a4b5b44bbd6f6534ab
Herve Desrosiers <hdesrosiers@mtlt30rjg5l-mac.corp.stingraydigital.com>
2019-04-18 12:15:08 -0400

    stingray machine welcome states

--- stingrayMusicMachine.js
+++ stingrayMusicMachine.js
@@ -1,18 +1,48 @@
-stingrayMusicMachine = {
+const welcomeMachine = Machine({
   key: "Stingray Music",
-  initial: "launching",
+  initial: "launch",
   states: {
-    launching: {
+    launch: {
       onEntry: ["loading splash"],
       on: {
         BYPASS_IP: "welcomePrison",
-        LOGIN_SUCCESS: "loggedIn",
-        LOGIN_FAILURE: "loggedOut"
+        LOGIN_SUCCESS: "accessMethod",
+        LOGIN_FAILURE: ""
+      }
+    },
+    accessMethod: {
+      on: {
+        KNOWN: "provider",
+        UNKNOWN: "territory"
+      }
+    },
+    provider: {
+      on: {
+        KNOWN: "providerMatch",
+        UNKNOWN: ""
+      }
+    },
+    providerMatch: {
+      on: {
+        MATCH: "player",
+        DIFFERENT: "switchProvider"
+      }
+    },
+    switchProvider: {
+      on: {
+        SWITCH_PROVIDER: "player",
+        CONTINUE_PROVIDER: "player"
+      }
+    },
+    territory: {
+      on: {
+        SUBSCRIPTION: "stingrayFreeSplash",
+        NO_SUBSCRIPTION: "stingraySplash"
       }
     },
     loggedIn: {
       on: {
-        LOG_OUT: "launching"
+        LOG_OUT: "launch"
       }
     },
     loggedOut: {
@@ -32,6 +62,18 @@ stingrayMusicMachine = {
       }
     },
     welcomeVIA: {},
-    providerSplash: {}
+    agreement: {
+      on: {
+        I_ACCEPT: "player"
+      }
+    },
+    providerSplash: {
+      on: {
+        CONTINUE: "player"
+      }
+    },
+    stingrayFreeSplash: {},
+    stingraySplash: {},
+    player: { type: "final" }
   }
-};
+});
